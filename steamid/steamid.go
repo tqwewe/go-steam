d2eb55945cdcd9b844fe184ad430c773027936e1
Ari Seyhun <ariseyhun@live.com.au>
2018-01-24 02:20:13 +1030

    Fix code comments

--- steamid/steamid.go
+++ steamid/steamid.go
@@ -5,35 +5,24 @@
 
 	Converting a SteamID
 
-	For simplicity, each SteamID type has the prefix "steam" removed resulting in each
-	type begging with "ID".
-
 	To convert a SteamID 64 bit (ID64) to a regular SteamID (ID) for example, you would
 	use the following code:
 
 		// Define type the SteamID 64 bit as type ID64.
-		id64 := steamid.ID64(76561198132612090)
+		id := steamid.NewID(STEAM_0:0:86173181)
 
 		// Convert id64 to a regular SteamID.
-		id := id64.ToID()
-
-	Although the code above is the preferred method for SteamID conversion (defining the
-	SteamID type and using it's conversion function), you may also use other functions to
-	convert using built-in types such as uint64 and string. You can accomplish this with
-	the following code:
-
-		// Convert a SteamID 64 bit to a regular SteamID.
-		id := steamid.FromIDTo64(76561198132612090)
+		id64 := id64.ToID64()
 
 
 	Finding an ID64 with a Search Query
 
 	ResolveID is a function available in this package that attempts to resolve a SteamID 64 from a
 	search query. It resolves queries by first identifying what the query is (eg. a vanity url) and
-	completes the required tasks to successfully resolve the query.
+	completes multiple tasks in an attempt successfully resolve the query.
 
 	You can use this function with a Steam web API key which is used to resolve the query
-	as a vanity URL, or, if you do not specify a valid API key (eg. an empty string), then in
+	as a vanity URL, or, if you do not specify a valid API key (an empty string), then in
 	most cases you will still be able to find the ID64 as long as the search query is a
 	SteamID in another format.
 
@@ -45,6 +34,7 @@
 */
 package steamid
 
+// ID is a regular SteamID. STEAM_0:0:86173181
 type ID interface {
 	String() string
 	To64() ID64
@@ -52,6 +42,7 @@ type ID interface {
 	To3() ID3
 }
 
+// ID64 is a SteamID 64 bit. 76561198132612090
 type ID64 interface {
 	Uint64() uint64
 	ToID() ID
@@ -59,6 +50,7 @@ type ID64 interface {
 	To3() ID3
 }
 
+// ID32 is a SteamID 32 bit. 172346362
 type ID32 interface {
 	Uint32() uint32
 	ToID() ID
@@ -66,6 +58,7 @@ type ID32 interface {
 	To3() ID3
 }
 
+// ID3 is a SteamID v3. [U:1:172346362]
 type ID3 interface {
 	String() string
 	ToID() ID
@@ -73,17 +66,12 @@ type ID3 interface {
 	To32() ID32
 }
 
-// ID is a regular SteamID. STEAM_0:0:86173181
-type steamID string
-
-// ID64 is a SteamID 64 bit. 76561198132612090
-type steamID64 uint64
-
-// ID32 is a SteamID 32 bit. 172346362
-type steamID32 uint32
-
-// ID3 is a SteamID v3. [U:1:172346362]
-type steamID3 string
+type (
+	steamID   string
+	steamID64 uint64
+	steamID32 uint32
+	steamID3  string
+)
 
 // NewID returns an ID from a string.
 func NewID(id string) ID {
